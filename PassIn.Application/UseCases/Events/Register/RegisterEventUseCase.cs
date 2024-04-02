73cd870f435d6dccaa3603e41ff672973f84a463
Ana Paula Leão <anaqueiroz.dev@gmail.com>
2024-04-01 21:58:08 -0300

    Adicionar arquivos de projeto.

---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,28 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Register;
+public class RegisterEventUseCase
+{
+    public void Execute(RequestEventJson request)
+    {
+        Validate(request);
+    }
+
+    public void Validate(RequestEventJson request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Title))
+        {
+            throw new ArgumentException("The title is invalid");
+        }
+
+        if (string.IsNullOrWhiteSpace(request.Details))
+        {
+            throw new ArgumentException("The details are invalid");
+        }
+
+        if(request.MaximumAttendees <= 0)
+        {
+            throw new ArgumentException("The maximun attendes is invalid");
+        }
+    }
+}
